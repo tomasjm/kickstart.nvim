11892fadabbc2f74546183e5cdcf0f0c3d5f19f9
Tomás Jiménez <tjimenez999@gmail.com>
2025-09-03 19:48:19 -0400

    keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -25,3 +25,23 @@ vim.keymap.set({ 'n', 'x' }, '<leader>gY', function()
     notify = false,
   }
 end, { desc = 'Git Browse (copy)' })
+
+-- LSP Keymaps
+vim.keymap.set('n', '<leader>cl', function() Snacks.picker.lsp_config() end, { desc = 'Lsp Info' })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Goto Definition' })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'References', nowait = true })
+vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { desc = 'Goto Implementation' })
+vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { desc = 'Goto T[y]pe Definition' })
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Goto Declaration' })
+vim.keymap.set('n', 'K', function() return vim.lsp.buf.hover() end, { desc = 'Hover' })
+vim.keymap.set('n', 'gK', function() return vim.lsp.buf.signature_help() end, { desc = 'Signature Help' })
+vim.keymap.set('i', '<c-k>', function() return vim.lsp.buf.signature_help() end, { desc = 'Signature Help' })
+vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
+vim.keymap.set({ 'n', 'v' }, '<leader>cc', vim.lsp.codelens.run, { desc = 'Run Codelens' })
+vim.keymap.set('n', '<leader>cC', vim.lsp.codelens.refresh, { desc = 'Refresh & Display Codelens' })
+vim.keymap.set('n', '<leader>cR', function() Snacks.rename.rename_file() end, { desc = 'Rename File' })
+vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Rename' })
+vim.keymap.set('n', ']]', function() Snacks.words.jump(vim.v.count1) end, { desc = 'Next Reference' })
+vim.keymap.set('n', '[[', function() Snacks.words.jump(-vim.v.count1) end, { desc = 'Prev Reference' })
+vim.keymap.set('n', '<a-n>', function() Snacks.words.jump(vim.v.count1, true) end, { desc = 'Next Reference' })
+vim.keymap.set('n', '<a-p>', function() Snacks.words.jump(-vim.v.count1, true) end, { desc = 'Prev Reference' })
